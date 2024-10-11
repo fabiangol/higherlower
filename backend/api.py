0797f7f51bfd08fe3f7748908f198cc18ccad635
Fabian Goldhorn <goldhorn.fabian@gmail.com>
2024-10-11 23:10:09 +0200

    Implement FastAPI endpoint /random-number
    /random-number literally returns a random number between 1 and 100

--- backend/api.py
+++ backend/api.py
@@ -1,2 +1,12 @@
 from fastapi import FastAPI
+import random
 
+app = FastAPI()
+
+@app.get("/random-number")
+def get_random_number():
+    return {"number": random.randint(1, 100)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
