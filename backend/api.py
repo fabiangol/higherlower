695ed7870410e664217d0fc192b428633f3f7f04
Fabian Goldhorn <goldhorn.fabian@gmail.com>
2024-10-16 21:12:58 +0200

    Add endpoint /mock-prompt
    mock-prompt returns a dict with "prompt" and "value" keys, both being the same random number.
    
    This endpoint is a way of assessing an existing game loop in a simple way

--- backend/api.py
+++ backend/api.py
@@ -8,5 +8,13 @@ app = FastAPI()
 def get_random_number():
     return {"number": random.randint(1, 100)}
 
+@app.get("/mock-prompt")
+def get_mock_prompt():
+    r = random.randint(1, 100)
+    return {
+        "prompt": r,
+        "value": r,
+    }
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
